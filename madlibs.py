1c608a96203fbd0fecf3d5745082f1a0d1b9dd0a
Ben Simpson <b.albertsimpson@gmail.com>
2019-09-03 00:03:02 -0700

    2nd Commit

--- madlibs.py
+++ madlibs.py
@@ -1,13 +1,14 @@
+
 word_list = list()
 
 def libs():
-    location = input("Where do you like to eat?: ")
+    location = input("Location of favorite place to eat: ")
     word_list.append(location)
     food = input("What is your favorite food?: ")
     word_list.append(food)
-    animal = input("What is the animal?: ")
+    animal = input("What is your favorite animal?: ")
     word_list.append(animal)
-    instrument = input("What is the instrument?: ")
+    instrument = input("What is your favorite instrument?: ")
     word_list.append(instrument)
     noun = input("Put in a noun: ")
     word_list.append(noun)
@@ -15,6 +16,5 @@ def libs():
     word_list.append(car)
 
 
-    print("I went to {} then cruised down the street in my {} to get some grub over at {}. Went to the {} to get the scoop. {} out there shooting some hoop. All I wanted to do was go home and play my {} but then quickly realized I didn't get enough {}. Headed back home and went back to sleep.".format(location, car, location, location, noun, instrument, animal))
+    print("I went to {} then cruised down the street in my {} to get some grub over at {}. Went to the {} to get the scoop. {} out there shooting some hoop. All I wanted to do was go home and play my {} but then quickly realized I didn't get enough {}. Headed back home and went back to sleep.".format( location, car, location, location, noun, instrument, animal))
 libs()
-
